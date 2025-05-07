837209dca83c9dce68472824b1c9d5b7e822bcf8
qtbodart <quentinboda@gmail.com>
2025-05-08 00:00:17 +0200

    Sync dotfiles - jeu. 08 mai 2025 00:00:17 CEST

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -1,32 +1,12 @@
 #!/bin/bash
 
-set -e
-
 DOTFILES_REPO="https://github.com/qtbodart/Arch-Dotfiles.git"
 DOTFILES_DIR="$HOME/.dotfiles"
 
-# Install git if needed
-if ! command -v git &> /dev/null; then
-    echo "Installing git..."
-    sudo pacman -Sy --noconfirm git
-fi
-
-# Clone dotfiles bare repo
-if [ ! -d "$DOTFILES_DIR" ]; then
-    echo "Cloning dotfiles repo..."
-    git clone --bare "$DOTFILES_REPO" "$DOTFILES_DIR"
-fi
-
-# Setup dotfiles alias
-alias dotfiles="/usr/bin/git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
-
-# Switch to SSH for future pushes
-dotfiles remote set-url origin git@github.com:qtbodart/Arch-Dotfiles.git
-
 # Checkout dotfiles
 echo "Checking out dotfiles..."
-dotfiles checkout -f
-dotfiles config --local status.showUntrackedFiles no
+git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" checkout -f
+git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" config --local status.showUntrackedFiles no
 
 # Source shell config
 if [ -f "$HOME/.bashrc" ]; then
